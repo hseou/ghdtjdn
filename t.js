598d074853a2a30a46030afa15d886b0ac561f0a
hseou <150679704+hseou@users.noreply.github.com>
2023-11-13 22:12:56 +0900

    Update t.js

---
+++ t.js
@@ -0,0 +1,61 @@
+const boardElement = document.getElementById('board');
+let currentPlayer = 'X';
+let board = ['', '', '', '', '', '', '', '', ''];
+let gameActive = true;
+
+function createBoard() {
+    for (let i = 0; i < 9; i++) {
+        const cell = document.createElement('div');
+        cell.className = 'cell';
+        cell.addEventListener('click', () => makeMove(i));
+        boardElement.appendChild(cell);
+    }
+}
+
+function makeMove(index) {
+    if (board[index] === '' && gameActive) {
+        board[index] = currentPlayer;
+        updateBoard();
+        checkWinner();
+        currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+    }
+}
+
+function updateBoard() {
+    const cells = document.querySelectorAll('.cell');
+    cells.forEach((cell, index) => {
+        cell.textContent = board[index];
+    });
+}
+
+function checkWinner() {
+    const winningCombinations = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8],
+        [0, 3, 6], [1, 4, 7], [2, 5, 8],
+        [0, 4, 8], [2, 4, 6]
+    ];
+
+    for (const combination of winningCombinations) {
+        const [a, b, c] = combination;
+        if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+            announceWinner(board[a]);
+            return;
+        }
+    }
+
+    if (!board.includes('')) {
+        announceDraw();
+    }
+}
+
+function announceWinner(winner) {
+    alert(`Player ${winner} wins!`);
+    gameActive = false;
+}
+
+function announceDraw() {
+    alert('It\'s a draw!');
+    gameActive = false;
+}
+
+createBoard();
